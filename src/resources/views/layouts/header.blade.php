59934228ac81d22c1e5bbe93223d4d7e417ad9c9
Saviur <martinkemeri2@gmail.com>
2025-09-18 16:34:01 +0200

    Fejléc elkészítése, bootstrap használatával, Layouts mappában header.blade.php néven

--- src/resources/views/layouts/header.blade.php
+++ src/resources/views/layouts/header.blade.php
@@ -28,17 +28,3 @@
     </div>
   </div>
 </nav>
-<h1 class="text-center">Home Page</h1>
-<footer class="footer theme-footer text-light py-3">
-  <div class="container text-center">
-    <p class="mb-1">© {{ date('Y') }} B+M Webáruház Minden jog fenntartva.</p>
-    <p class="mb-0">
-      <a href="/aszf" class="text-light">ÁSZF</a> |
-      <a href="/adatvedelem" class="text-light">Adatvédelem</a>
-    </p>
-  </div>
-</footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  </body>
-</html>
